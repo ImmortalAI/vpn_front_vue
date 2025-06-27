a68353e0eaa038a96f965b72350cff0416612aa9
ImmortalAI <moniki2139@yandex.ru>
2025-06-28 00:23:08 +0300

    add JSDoc to user api

--- src/api/user/service.ts
+++ src/api/user/service.ts
@@ -1,7 +1,21 @@
 import apiClient from '@/utils/apiClient';
-import { UserAllGetRsSchema, UserGetRsSchema, UserPatchRsSchema, type UserPatchRq } from './schema';
+import {
+  UserAllGetRsSchema,
+  UserGetRsSchema,
+  UserPatchRsSchema,
+  type UserAllGetRs,
+  type UserGetRs,
+  type UserPatchRq,
+  type UserPatchRs,
+} from './schema';
 
-export async function userGet() {
+/**
+ * Get the data of the current authorized user
+ *
+ * @returns {Promise<UserGetRs>} User data.
+ * @throws If the request fails.
+ */
+export async function userGet(): Promise<UserGetRs> {
   try {
     const res = await apiClient.get('/user');
     return UserGetRsSchema.parse(res.data);
@@ -10,7 +24,15 @@ export async function userGet() {
   }
 }
 
-export async function userPatch(userId: string, request: UserPatchRq) {
+/**
+ * Update the user data for a specific user UUID.
+ *
+ * @param {string} userId - The UUID of the user to update.
+ * @param {UserPatchRq} request - The request object containing the user data to update.
+ * @returns {Promise<UserPatchRs>} The response data with status message.
+ * @throws If the request fails.
+ */
+export async function userPatch(userId: string, request: UserPatchRq): Promise<UserPatchRs> {
   try {
     const res = await apiClient.patch(`/user/${userId}`, request);
     return UserPatchRsSchema.parse(res.data);
@@ -19,7 +41,13 @@ export async function userPatch(userId: string, request: UserPatchRq) {
   }
 }
 
-export async function userAll() {
+/**
+ * Get the list of all users data.
+ *
+ * @returns {Promise<UserAllGetRs>} An array of user data.
+ * @throws If the request fails.
+ */
+export async function userAll(): Promise<UserAllGetRs> {
   try {
     const res = await apiClient.get('/user/all');
     return UserAllGetRsSchema.parse(res.data);
