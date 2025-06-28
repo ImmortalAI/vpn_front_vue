6d871ac7167cf4a2336ada1e5de2a980e24fb857
ImmortalAI <moniki2139@yandex.ru>
2025-06-28 13:28:39 +0300

    refresh auth on app creation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,3 @@
-<script setup lang="ts"></script>
-
 <template>
   <header>
     <img alt="Vue logo" src="@/assets/logo.svg" width="125" height="125" />
@@ -13,3 +11,30 @@
 
   <Toast position="bottom-right" />
 </template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import apiClient from './utils/apiClient';
+import { useUserStore } from './stores/user';
+import { useRouter } from 'vue-router';
+import { isAxiosError } from 'axios';
+import useErrorToast from './composables/useErrorToast';
+
+onMounted(async () => {
+  const user = useUserStore();
+  const router = useRouter();
+  const errorToast = useErrorToast();
+
+  try {
+    await apiClient.post('/auth/refresh');
+    await user.refreshUser();
+    router.push('/dashboard');
+  } catch (e) {
+    if (isAxiosError(e)) {
+      errorToast.error(e);
+    } else {
+      throw e;
+    }
+  }
+})
+</script>
