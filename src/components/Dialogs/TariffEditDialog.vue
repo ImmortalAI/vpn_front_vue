17c9951df22a6a30bdc1b9286669e33332514e71
ImmortalAI <moniki2139@yandex.ru>
2026-03-08 00:04:04 +0300

    fix(tariff): add missing ID field to modal window

--- src/components/Dialogs/TariffEditDialog.vue
+++ src/components/Dialogs/TariffEditDialog.vue
@@ -1,13 +1,14 @@
 <template>
-  <Dialog
-    v-model:visible="visible"
-    modal
+  <Dialog v-model:visible="visible" modal
     :header="tariffInEdit.id == '' ? 'Create New Tariff' : 'Edit Tariff => ' + tariffInEdit.name"
-    class="w-[60vw] h-[80vh]"
-  >
+    class="w-[60vw] h-[80vh]">
     <Fluid class="flex flex-col gap-4">
       <div class="flex flex-col gap-4">
         <span class="text-xl p-2 border-b border-neutral-600">Main Settings</span>
+        <FloatLabel class="float-label-spacer" variant="in">
+          <InputText inputId="id-input" v-model="tariffInEdit.id" disabled />
+          <label for="id-input">Tariff Id</label>
+        </FloatLabel>
         <FloatLabel class="float-label-spacer" variant="in">
           <InputText inputId="name-input" v-model="tariffInEdit.name" />
           <label for="name-input">Tariff Name</label>
@@ -21,49 +22,28 @@
         <span class="text-xl p-2 border-b border-neutral-600">Parameters</span>
         <div class="flex gap-4">
           <FloatLabel class="float-label-spacer" variant="in">
-            <InputNumber
-              inputId="duration-input"
-              v-model="tariffInEdit.duration"
-              :useGrouping="false"
-              :min="0"
-              :max="65535"
-            />
+            <InputNumber inputId="duration-input" v-model="tariffInEdit.duration" :useGrouping="false" :min="0"
+              :max="65535" />
             <label for="duration-input">Duration in days</label>
           </FloatLabel>
           <FloatLabel class="float-label-spacer" variant="in">
-            <InputNumber
-              inputId="traffic-input"
-              v-model="tariffInEdit.traffic"
-              :useGrouping="false"
-              :min="0"
-              suffix=" GiB"
-            />
+            <InputNumber inputId="traffic-input" v-model="tariffInEdit.traffic" :useGrouping="false" :min="0"
+              suffix=" GiB" />
             <label for="traffic-input">Traffic in GiB</label>
           </FloatLabel>
         </div>
         <div class="flex gap-4">
           <FloatLabel class="float-label-spacer" variant="in">
-            <InputNumber
-              inputId="price-input"
-              :modelValue="tariffInEdit.price / 100"
-              @update:modelValue="(val) => (tariffInEdit.price = Math.round(val * 100))"
-              mode="currency"
-              currency="RUB"
-              locale="ru-RU"
-            />
+            <InputNumber inputId="price-input" :modelValue="tariffInEdit.price / 100"
+              @update:modelValue="(val) => (tariffInEdit.price = Math.round(val * 100))" mode="currency" currency="RUB"
+              locale="ru-RU" />
             <label for="price-input">Price in RUB</label>
           </FloatLabel>
           <FloatLabel class="float-label-spacer" variant="in">
-            <InputNumber
-              inputId="price-reset-input"
-              :modelValue="tariffInEdit.price_of_traffic_reset / 100"
+            <InputNumber inputId="price-reset-input" :modelValue="tariffInEdit.price_of_traffic_reset / 100"
               @update:modelValue="
                 (val) => (tariffInEdit.price_of_traffic_reset = Math.round(val * 100))
-              "
-              mode="currency"
-              currency="RUB"
-              locale="ru-RU"
-            />
+              " mode="currency" currency="RUB" locale="ru-RU" />
             <label for="price-reset-input">Reset Price in RUB</label>
           </FloatLabel>
         </div>
@@ -76,11 +56,7 @@
             <label for="with-access-input">With access</label>
           </div>
           <div class="flex items-center gap-2">
-            <Checkbox
-              inputId="with-un-inbounds-input"
-              v-model="tariffInEdit.with_unavailable_inbounds"
-              binary
-            />
+            <Checkbox inputId="with-un-inbounds-input" v-model="tariffInEdit.with_unavailable_inbounds" binary />
             <label for="with-un-inbounds-input">With unavailable inbounds</label>
           </div>
           <div class="flex items-center gap-2">
